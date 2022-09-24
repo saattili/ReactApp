90fb4231a8234159365ad75bb6cd6ad05c79ef25
saattili <attsmadhav@gmail.com>
2022-09-24 09:39:50 +0530

    commented the content import

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Header from './Header';
 import Employees from './Employee';
-import Content from './Content';
+//import Content from './Content';
 import Footer from './Footer';
 
 export default function App() {
